5785df42c2d3f331e7f15933c974f7fdc3100f9a
coderTech11 <hubcode1@gmail.com>
2024-04-27 22:23:56 +0545

    first commit

---
+++ Bank Console Application/IAccount.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Bank_Console_Application
+{
+   internal interface IAccount
+    {
+        //represents common operations for all accounts
+        string AccountNumber { get; }
+
+        string AccountHolder { get; }
+
+        double Balance { get; }
+
+        IReadOnlyList<Transaction> Transactions { get; }
+
+        void Deposit(double amount, string description);
+
+        void Withdraw(double amount, string description);
+
+        void DisplayAccountInfo();
+
+    }
+}
---
+++ Bank Console Application/Transaction.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Bank_Console_Application
+{
+   public  class Transaction
+    {
+        public string? Description { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
